7361e43dcdc7d18f536ba60da499a7b1873238a7
polotud10 <paul.tudou@gmail.com>
2014-04-15 13:58:37 +0200

    ajout de la coorddonée Y dans DessinVoiture et VueVoiture

--- src/applicationSimulateur/DessinVoiture.java
+++ src/applicationSimulateur/DessinVoiture.java
@@ -10,6 +10,7 @@ public class DessinVoiture extends JFrame {
 	public static final int TailleFenetreEnPixels = 500;
 	
 	private int xPixelVoiture;
+	private int yPixelVoiture;
 
 	public DessinVoiture() {
 		super();
@@ -17,22 +18,27 @@ public class DessinVoiture extends JFrame {
 		this.setSize(TailleFenetreEnPixels, TailleFenetreEnPixels);
 		this.setVisible(true);
 		this.xPixelVoiture = 0;
+		this.yPixelVoiture = 0;
 	}
 
 	@Override
 	public void paint(Graphics graphics) {
 		super.paint(graphics);
-		dessinerVoiture(this.xPixelVoiture, graphics);
+		dessinerVoiture(this.xPixelVoiture, this.yPixelVoiture, graphics);
 
 	}
 
-	public void dessinerVoiture(int xPixelVoiture, Graphics graphics) {
-		graphics.fillRect(xPixelVoiture, 200, 40, 20);
+	public void dessinerVoiture(int xPixelVoiture, int yPixelVoiture, Graphics graphics) {
+		graphics.fillRect(xPixelVoiture, yPixelVoiture, 40, 20);
 
 	}
 
 	public void setXPixelVoiture(int xPixelVoiture) {
 		this.xPixelVoiture = xPixelVoiture;
+	}
+	
+	public void setYPixelVoiture(int yPixelVoiture) {
+		this.yPixelVoiture = yPixelVoiture;
 	}
 
 }
--- src/applicationSimulateur/VueVoiture.java
+++ src/applicationSimulateur/VueVoiture.java
@@ -43,7 +43,10 @@ public class VueVoiture implements Observer {
 
 		int xVoiture = this.voiture.getCoordXEnMetres();
 		int xPixelVoiture = this.transformerMetrePixel(xVoiture);
+		int yVoiture = this.voiture.getCoordYEnMetres();
+		int yPixelVoiture = this.transformerMetrePixel(yVoiture);
 		ihm.setXPixelVoiture(xPixelVoiture);
+		ihm.setYPixelVoiture(yPixelVoiture);
 		ihm.repaint();
 
 	}
