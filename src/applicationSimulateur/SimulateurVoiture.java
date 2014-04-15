bc854629f5ffd818f58bc51dab52445035b32822
polotud10 <paul.tudou@gmail.com>
2014-04-15 16:15:25 +0200

    fleches directionnelles opérationnelles

--- src/applicationSimulateur/SimulateurVoiture.java
+++ src/applicationSimulateur/SimulateurVoiture.java
@@ -2,6 +2,8 @@ package applicationSimulateur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.Timer;
 
@@ -28,6 +30,42 @@ public class SimulateurVoiture {
 		});
 		
 		timerAvancer.start();
+		
+		
+		dessinVoiture.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					System.out.println("fleche du haut");
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					System.out.println("fleche du bas");
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					System.out.println("fleche de droite");
+				}
+				
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					System.out.println("fleche de gauche");
+				}
+			}
+		});
 		
 		while(true){
 		}
